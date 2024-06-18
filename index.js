8903fce82713dbf853babaaaada70ba8687c5b3a
atitp07 <atit007panchal@gmail.com>
2024-06-18 16:47:38 +0530

    JS

---
+++ index.js
@@ -0,0 +1,44 @@
+// const a1 ={
+//   name : "Atit",
+//   section : 1,
+//   isPrincipal : false
+// }
+
+// a1['begin'] = "Atit"
+// a1['name'] = "Ap"
+// console.log(a1);
+
+
+// let a = "Atit"
+// let b = 6
+// let c = a+b;
+// console.log(typeof(a+b));
+
+// let a = 145;
+// let b = 4;
+// console.log("a+b = ", a+b)
+// console.log("a-b = ", a-b)
+// console.log("a/b = ", a/b)
+// console.log("a*b = ", a*b)
+// console.log("a%b = ", a%b)
+// console.log("a ** b = ", a**b)
+// console.log("++a = ", ++a)
+// console.log("a++ = ", a++)
+// console.log("--a = ", --a)
+// console.log("a-- = ", a--)
+// console.log("a = ", a)
+// console.log("a-- = ", a-- )
+
+// let x = 11;
+// x += 5;
+// console.log("x is now =", x)
+// x -= 5;
+// console.log("x is now =", x)
+// x *= 5;
+// console.log("x is now =", x)
+// x /= 5;
+// console.log("x is now =", x)
+
+let a = prompt("Hey whats your age?")
+a = Number.parseInt(a)
+console.log(typeof a)  
